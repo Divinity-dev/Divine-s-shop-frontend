cdac04af55c57a7d109534cdf9299a880323018f
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-09-08 23:00:58 +0100

    add css

--- e-commerce/src/components/Navbar.jsx
+++ e-commerce/src/components/Navbar.jsx
@@ -5,8 +5,17 @@ const Navbar = () => {
   return (
     <div className='container'>
         <div className='wrapper'>
-           <div className='left'><span>EN</span></div>
-           <div className='center'>center</div>
+           <div className='left'>
+            <span>EN</span>
+            <div className='search'>
+            <input />
+            <ion-icon name="search-outline"></ion-icon>
+            </div>
+            
+            </div>
+           <div className='center'>
+              center
+           </div>
            <div className='right'>right</div>
         </div>
       
