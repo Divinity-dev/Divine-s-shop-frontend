7586260f0cbf2debf18a2fc2889ddff44d2c1e95
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-11-30 21:34:04 +0100

    add css styling to slidder

--- e-commerce/src/components/Slidder.jsx
+++ e-commerce/src/components/Slidder.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import  './Slidder.css'
+import React from 'react';
+import './Slidder.css';
 
 const Slidder = () => {
   return (
-    <div className='arrows'>
-      <div className='back' >
-      <ion-icon name="arrow-back-circle-outline" ></ion-icon>
+    <div className='items'>
+      <div className='back'>
+          <ion-icon name="arrow-back-circle-outline" ></ion-icon>
+        </div>
+        <div className='forward'>
+          <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+        </div> 
+      <div className='slidder-item'>
+        <img src="https://i.ibb.co/DG69bQ4/2.png" alt="" className='slidder-img' />
+        <div className='slidder-desc'>
+          <h1>summer sale</h1>
+          <p>Dress available now at discounted rate</p>
+          <button>shop now</button>
+        </div>
       </div>
-      <div className='forward'>
-      <ion-icon name="arrow-forward-circle-outline"></ion-icon>
-      </div> 
-    </div>
-  )
+        
+      </div>
+    
+  );
 }
 
-export default Slidder
+export default Slidder;
