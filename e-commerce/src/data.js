709d5bf7f666188a5def03078fbb1fac25ca1184
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-12-02 22:57:24 +0100

    Added an image

--- e-commerce/src/data.js
+++ e-commerce/src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://i.ibb.co/XsdmR2c/1.png",
+      img: "https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
