28e085a811fc44c118aa763e9720d0cf24d6103d
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-12-25 23:00:40 +0100

    completed single product page

--- e-commerce/src/components/Singleproduct.jsx
+++ e-commerce/src/components/Singleproduct.jsx
@@ -8,7 +8,8 @@ const Singleproduct = () => {
          <img src="https://i.ibb.co/S6qMxwr/jean.jpg" alt="" />
       </div>
       <div className='desc'>
-         <h1>Jump suit</h1>
+        <div>
+             <h1>Jump suit</h1>
          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -17,6 +18,34 @@ const Singleproduct = () => {
                Letraset sheets containing Lorem Ipsum passages, and more recently
              with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
          <span>Price: $200</span>
+        </div>
+        <div className='select'>
+            <div className='color'>
+               <span>Color</span>
+               <div className='div div1'></div>
+               <div className='div div2'></div>
+               <div className='div div3'></div>
+            </div>
+            <div className='size'>
+            <select name="" id=""  className='size'>
+            <option value="" disabled selected >Size</option>
+            <option value="">XS</option>
+            <option value="">S</option>
+            <option value="">M</option>
+            <option value="">L</option>
+            <option value="">XL</option>
+            
+        </select>
+            </div>
+        </div>
+        <div className='count'>
+          <div>
+          <ion-icon name="add-outline"></ion-icon>
+          <span>1</span>
+          <ion-icon name="remove-outline"></ion-icon>
+          </div>
+          <button>Add to cart</button>
+        </div>
       </div>
     </div>
   )
