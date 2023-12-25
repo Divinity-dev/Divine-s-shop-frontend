70d1e161eefec68f6158c61c79892eff59f97423
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-12-26 00:49:12 +0100

    finish the login page

--- e-commerce/src/pages/Login.jsx
+++ e-commerce/src/pages/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='register'>
+       <div className='registercontainer2'>
+        <h1>SIGN IN</h1>
+      <form action="" className='holder2'>
+        <div className='form'>
+        <input type="text"  placeholder=' username'/>
+        <input type="text"  placeholder='password'/>
+        </div>
+        <button>sign in</button>
+        <Link to=''>Do not remember password?</Link>
+        <Link to=''>Create account</Link>
+      </form>
+    </div>
     </div>
   )
 }
