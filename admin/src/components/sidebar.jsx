f751cd3294f7e2d97e4027539d912f610920106e
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-02-02 12:37:07 +0100

    Add more components

--- admin/src/components/sidebar.jsx
+++ admin/src/components/sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {LineStyle, Timeline, TrendingUp} from '@mui/icons-material'
+import {LineStyle, Timeline, TrendingUp, PersonOutline,Inventory2, AttachMoney,AssessmentOutlined, MailOutline, Forum, Chat, ManageAccounts} 
+from '@mui/icons-material'
 
 const Sidebar = () => {
   return (
@@ -8,41 +9,87 @@ const Sidebar = () => {
         <h3>Dashboard</h3>
         <ul className='list active'>
           <li>
-            <LineStyle/>
+            <LineStyle className='indicators'/>
             Home
           </li>
         </ul>
         <ul className='list'>
           <li>
-            <Timeline/>
+            <Timeline className='indicators'/>
             Analytics
           </li>
         </ul>
         <ul className='list'>
           <li>
-            <TrendingUp/>
+            <TrendingUp className='indicators'/>
             Sales
           </li>
         </ul>
       </div>
       <div className='listcontainer'>
-        <h3>Dashboard</h3>
+        <h3>Quickmenu</h3>
         <ul className='list'>
           <li>
-            <LineStyle/>
-            Home
+            <PersonOutline className='indicators'/>
+            Users
           </li>
         </ul>
         <ul className='list'>
           <li>
-            <Timeline/>
+            <Inventory2 className='indicators'/>
+           Products
+          </li>
+        </ul>
+        <ul className='list'>
+          <li>
+            <AttachMoney className='indicators'/>
+            Transactions
+          </li>
+          <li>
+            <AssessmentOutlined className='indicators'/>
+            Reports
+          </li>
+        </ul>
+      </div>
+      <div className='listcontainer'>
+        <h3>Notiication</h3>
+        <ul className='list'>
+          <li>
+            <MailOutline className='indicators'/>
+            Mail
+          </li>
+        </ul>
+        <ul className='list'>
+          <li>
+            <Forum className='indicators'/>
+            Feedback
+          </li>
+        </ul>
+        <ul className='list'>
+          <li>
+            <Chat className='indicators'/>
+            Messages
+          </li>
+        </ul>
+      </div>
+      <div className='listcontainer'>
+        <h3>Staff</h3>
+        <ul className='list'>
+          <li>
+            <ManageAccounts className='indicators'/>
+           Manage
+          </li>
+        </ul>
+        <ul className='list'>
+          <li>
+            <Timeline className='indicators'/>
             Analytics
           </li>
         </ul>
         <ul className='list'>
           <li>
-            <TrendingUp/>
-            Sales
+            <AssessmentOutlined className='indicators'/>
+            Report
           </li>
         </ul>
       </div>
