d29c4a77ace09e2d35c1b51adf8bc60b52cde17a
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-02-13 21:54:54 +0100

    completed the design for membership section

--- admin/src/components/Smallwg.jsx
+++ admin/src/components/Smallwg.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import {Visibility} from '@mui/icons-material'
 
 const Smallwg = () => {
   return (
     <div className='smwg'>
-      smg
+      <span className="members">Newly joined members</span>
+      <div className='memberproile'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbfDDvvP-Ui1mHwsJP1F7QCKZDbs51d2oWVKBtVxWOUw&s" alt="" className="photo" />
+      <div className='spans'>
+      <span className="name">Tracy Asiriuwa</span>
+      <span className="job">Sotware engineer</span>
+      </div>
+      <button className='visibility'>
+      <Visibility className='vis'/>
+      Display
+      </button>
+      </div>
+      <div className='memberproile'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbfDDvvP-Ui1mHwsJP1F7QCKZDbs51d2oWVKBtVxWOUw&s" alt="" className="photo" />
+      <div className='spans'>
+      <span className="name">Tracy Asiriuwa</span>
+      <span className="job">Sotware engineer</span>
+      </div>
+      <button className='visibility'>
+      <Visibility/>
+      Display
+      </button>
+      </div>
+      <div className='memberproile'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbfDDvvP-Ui1mHwsJP1F7QCKZDbs51d2oWVKBtVxWOUw&s" alt="" className="photo" />
+      <div className='spans'>
+      <span className="name">Tracy Asiriuwa</span>
+      <span className="job">Sotware engineer</span>
+      </div>
+      <button className='visibility'>
+      <Visibility/>
+      Display
+      </button>
+      </div>
+      <div className='memberproile'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbfDDvvP-Ui1mHwsJP1F7QCKZDbs51d2oWVKBtVxWOUw&s" alt="" className="photo" />
+      <div className='spans'>
+      <span className="name">Tracy Asiriuwa</span>
+      <span className="job">Sotware engineer</span>
+      </div>
+      <button className='visibility'>
+      <Visibility/>
+      Display
+      </button>
+      </div>
+      <div className='memberproile'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbfDDvvP-Ui1mHwsJP1F7QCKZDbs51d2oWVKBtVxWOUw&s" alt="" className="photo" />
+      <div className='spans'>
+      <span className="name">Tracy Asiriuwa</span>
+      <span className="job">Sotware engineer</span>
+      </div>
+      <button className='visibility'>
+      <Visibility/>
+      Display
+      </button>
+      </div>
+      
     </div>
   )
 }
