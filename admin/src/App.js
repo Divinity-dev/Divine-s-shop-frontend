0c745dcc2499d70737f2be0fc39c9c71a51fd8ab
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-02-17 23:54:07 +0100

    Add a table

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,17 +2,26 @@ import './App.css';
 import Topbar from './components/Topbar'
 import Sidebar from './components/sidebar';
 import Home from './pages/home';
+import Userlist from './pages/Userlist';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Topbar/>
       <div className='maincomtainer'>
          <Sidebar/>
          <div className='center'>
-           <Home/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/users' element={<Userlist/>}/>
+          </Routes>
+           
          </div>
       </div>
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ admin/src/pages/Userlist.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+
+const Userlist = () => {
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'user', headerName: 'User', width: 200, renderCell: (params)=>{
+          return (<div className='displaypicture'>
+                  <img src={params.row.avatar} alt=""  className='displayimage'/>
+                  {params.row.username}
+          </div>)
+        } },
+        { field: 'email', headerName: 'Email', width: 130 },
+        {
+          field: 'status',
+          headerName: 'Status',
+          width: 90,
+        },
+        {
+          field: 'transaction',
+          headerName: 'Transaction',
+          width: 90,
+        },
+      ];
+      
+      const rows = [
+        { id: 1, username: 'Divine Asiriuwa', avatar:'https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg',
+      email:'divineaasiriuwa@gmail.com', status:'active', transaction:'$120' },
+      { id: 2, username: 'Divine Asiriuwa', avatar:'https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg',
+      email:'divineaasiriuwa@gmail.com', status:'active', transaction:'$120' },
+      { id: 3, username: 'Divine Asiriuwa', avatar:'https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg',
+      email:'divineaasiriuwa@gmail.com', status:'active', transaction:'$120' },
+      { id: 4, username: 'Divine Asiriuwa', avatar:'https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg',
+      email:'divineaasiriuwa@gmail.com', status:'active', transaction:'$120' },
+      { id: 5, username: 'Divine Asiriuwa', avatar:'https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg',
+      email:'divineaasiriuwa@gmail.com', status:'active', transaction:'$120' },
+      { id: 6, username: 'Divine Asiriuwa', avatar:'https://as2.ftcdn.net/v2/jpg/01/06/24/39/1000_F_106243915_97MVJgRC25acsXIpGDwyAzMyNbgoUKoT.jpg',
+      email:'divineaasiriuwa@gmail.com', status:'active', transaction:'$120' },
+      
+      ];
+      
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  )
+}
+
+export default Userlist
